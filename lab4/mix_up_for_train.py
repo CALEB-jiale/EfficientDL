642a8d3699a85a84f6c20bdc34fad730948d84ff
Xianzhi YE <x21ye@fl-tp-br-518.imta.fr>
2023-03-08 11:11:52 +0100

    basic

--- lab4/mix_up_for_train.py
+++ lab4/mix_up_for_train.py
@@ -3,9 +3,12 @@ import numpy as np
 import torchvision.transforms as transforms
 import torch
 from torch.utils.data.dataloader import DataLoader
+import torch.backends.cudnn as cudnn
 import torch.nn as nn
+import os
 import torch.optim as optim
 import warnings
+import argparse
 import sys
 from mix_up import *
 sys.path.append("pytorch-cifar-master")
@@ -18,6 +21,7 @@ PATH = '/homes/x21ye/Documents/Efficient DL'
 
 #  The data from CIFAR10 will be downloaded in the following folder
 data_dir = PATH + '/lab1/data_set/cifar10'
+data_checkpoint = PATH + '/lab4//checkpoint/ckpt.pth'
 
 
 def load_data(data_dir):
@@ -113,7 +117,19 @@ def test(epoch):
             tot_time = progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
+    # Save checkpoint.
     acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, data_checkpoint)
+        best_acc = acc
 
     return acc, test_loss/(batch_idx+1), tot_time
 
@@ -125,8 +141,30 @@ trainloader, testloader = load_data(data_dir)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+args = parser.parse_args()
+
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
 net = DenseNet121()
 net = net.to(device)
+
+if device == 'cuda':
+    net = torch.nn.DataParallel(net)
+    cudnn.benchmark = True
+
+if args.resume:
+    # Load checkpoint.
+    print('==> Resuming from checkpoint..')
+    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+    checkpoint = torch.load('./checkpoint/ckpt.pth')
+    net.load_state_dict(checkpoint['net'])
+    best_acc = checkpoint['acc']
+    start_epoch = checkpoint['epoch']
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=5e-4)
